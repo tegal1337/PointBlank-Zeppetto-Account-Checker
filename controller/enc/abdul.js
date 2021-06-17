ccd32cf9fada4d0ce77341372fa1eaf4d409aa77
Abdul Muttaqin <abdulmuttaqin456@gmail.com>
2021-06-17 11:58:43 +0700

    Update abdul.js

--- controller/enc/abdul.js
+++ controller/enc/abdul.js
@@ -1,10 +1,1 @@
-var fdciabdul = require('javascript-obfuscator');
-const atob = require("atob")
-var sayangdina = atob("QWJkdWwgTXV0dGFxaW4=");
 
-    function iniabdul(){
-        var dina = fdciabdul.obfuscate(sayangdina)
-        console.log(dina)
-        return dina;
-    }
-exports.iniabdul = iniabdul;
